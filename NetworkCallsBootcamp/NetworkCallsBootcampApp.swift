41665183a8e5d5d59d329b89a911edb395e84e9c
Dhrushit Raval <dr9703@rit.edu>
2024-07-23 16:53:00 -0700

    Initial Commit

---
+++ NetworkCallsBootcamp/NetworkCallsBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallsBootcampApp.swift
+//  NetworkCallsBootcamp
+//
+//  Created by Dhrushit Raval on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallsBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
